838d6bd290164fcf34445294968ef65d866b8735
Amir Kishk <kishk.amir@gmail.com>
2023-09-23 15:21:07 +0200

    commit end of phase one (added book form Edit)

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
-  const deleteBookByID = (id) => {
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id;
-    });
-
-    setBooks(updatedBooks);
-  };
-
   const CreateBook = (title) => {
     const updatedBooks = [
       ...books,
@@ -23,9 +15,30 @@ function App() {
     setBooks(updatedBooks);
   };
 
+  const editBookByID = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+
+      return book;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookByID = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  };
+
   return (
     <div className="app">
-      <BookList books={books} onDelete={deleteBookByID} />
+      <h1>Reading List</h1>
+      <BookList books={books} onEdit={editBookByID} onDelete={deleteBookByID} />
       <BookCreate onCreate={CreateBook} />
     </div>
   );
